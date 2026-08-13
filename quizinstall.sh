8174b88b4d8e4108e788fe8ea4a61898df213a46
gurunathan <guru@gegosoft.com>
2026-08-13 11:48:52 +0530

    Added video& quiz install file

---
+++ quizinstall.sh
@@ -0,0 +1,9 @@
+php artisan vendor:publish --tag=quiz-routes --force
+
+php artisan vendor:publish --tag=quiz-components --force
+
+php artisan vendor:publish --tag=quiz-views --force
+
+php artisan vendor:publish --tag=quiz-migrations --force
+
+php artisan vendor:publish --tag=quiz-config --force
---
+++ videoinstall.sh
@@ -0,0 +1,11 @@
+php artisan vendor:publish --tag=videoroom-routes --force
+
+php artisan vendor:publish --tag=videoroom-components --force
+
+php artisan vendor:publish --tag=videoroom-views --force
+
+php artisan vendor:publish --tag=videoroom-migrations --force
+
+php artisan vendor:publish --tag=videoroom-config --force
+
+php artisan vendor:publish --tag=videoroomapi-routes --force
