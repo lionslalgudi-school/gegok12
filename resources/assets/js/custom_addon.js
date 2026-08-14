fc9faca8837af748502fc55e371c7ce68debf360
lionslalgudi-school <lionslalgudigego26@gmail.com>
2026-08-14 18:25:50 +0530

    Changes custom addon

--- resources/assets/js/custom_addon.js
+++ resources/assets/js/custom_addon.js
@@ -1,7 +1,24 @@
+import { registerCertificate } from './gcertificate'
+import { registerTimetable } from './gtimetable'
+import { registerQuiz } from './gquiz'
+import { registerConference } from './gvideoroom'
+import { registerInventory } from './ginventory'
+import { registerFees } from './gfees'
 
 // later you can import:
 
 export default function registerCustomAddon(app) {
+
+    registerTimetable(app)
+
+    registerQuiz(app)
+
+    registerConference(app)
+
+    registerInventory(app)
+
+    registerCertificate(app)
+    registerFees(app)
 
 
   
